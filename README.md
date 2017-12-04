0631e761919529f0874cb5eca6b9ff1c63aa3be8
Andrew Cho <andrewopes789@gmail.com>
2017-12-04 09:00:58 -0800

    Wireframes and updated README

--- README.md
+++ README.md
@@ -24,11 +24,11 @@ In Restate, users will be able to:
 
 * Select an area to retrieve real estate analytics for
 * Select which analytics parameters are mapped on the graph
-* Specify the type(s) of property (i.e. single family, condominium, townhouse) to retrieve analytics for
-* Specify the timeframe to receive analytics for, segmented by month, dating back to April 2015
+* Specify the timeframe to receive analytics for, segmented by month, dating back to 2011
 
 In addition, this project will include:
 
+* An initial modal to select a zip code for the search
 * An About modal describing the basic functionality
 
 ### Wireframes
@@ -42,6 +42,11 @@ This project will be implemented with the following technologies:
 * `PropMiX API` for real-time real estate analytics,
 * Webpack to bundle and serve up the various scripts.
 
+In addition to the webpack entry file, there will be one script involved in this project:
+
+`script.js`: this script will handle the logic for creating and updating the necessary filters for the graph.
+`chart.js`: this script will handle the render for creating the graph.
+
 ### Implementation Timeline
 
 #### Over the weekend:
@@ -49,12 +54,32 @@ This project will be implemented with the following technologies:
 * Familiarize myself with Chart.js and PropMiX
 
 #### Day 1: 
+Get webpack up and running. Create `webpack.config.js` as well as `package.json.` Write a basic entry file and the bare bones
+of the scripts outline above. Learn the basics of the API and how to fetch the data. Goals for the day: 
+
+* Get `webpack` serving files and frame out index.html
+* Learn enough about the API to properly fetch data with the api key
+* Learn about `Chart.js` and start framing it out.
 
 #### Day 2:
+Build out the scripts and the chart filters to change the path in response to user input. Goals for the day: 
+
+* Finish building out the relevant scripts 
+* Make the app make a new API call each time the user changes the location or date of his/her search
 
 #### Day 3:
+Connect the chart filters and the chart and have the chart be responsive in real-time to the user's selections. Goals for the day:
 
-#### Day 4:
+* Get the chart up to display all the paramaters fetched by the API call
+* Get the chart to re-render in real time every time the user changes the parameters of the query
+* Create the About modal
+
+#### Day 4: 
+Style the frontend, making it polished and professional. Goals for the day: 
+
+* Style the initial city selection modal
+* Style the graph and filters box
+* Style the About modal
 
 ### Bonus Features
 There are many directions in which this project could evolve.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,13 @@
 $(function () {
+
+  let area = [];
   let filters = {};
 
-  const checkboxes = $('.all-analytics input[type=checkbox]');
+  const checkboxes = $(".all-analytics input[type=checkbox]");
 
   checkboxes.click(function () {
     let that = $(this);
-    let specName = that.attr('name');
+    let specName = that.attr("name");
 
     if(that.is(":checked")) {
 
@@ -37,16 +39,102 @@ $(function () {
     }
   });
 
+  $(".filters button").click(function(e) {
+    e.preventDefault();
+    window.location.hash = "#";
+  });
+  // 
+  // let selectArea = $('.select-area');
+  //
+  // selectArea.on('click', function (e) {
+  //   if (selectArea.hasClass('visible')) {
+  //     let clicked = $(e.target);
+  //   }
+  // });
+
+  $(window).on('hashchange', function() {
+    render(decodeURI(window.location.hash));
+  });
+
+  function render(url) {
+    let temp = url.split("/")[0];
+
+    $(".main-content .page").removeClass("visible");
+
+    let map = {
+      '': function() {
+        renderAreaPage();
+      },
+      '#chart': function() {
+
+        filters = {};
+        checkboxes.prop("checked", false);
+
+        renderAnalyticsChart(area);
+      },
+
+      '#filter': function() {
+        url = url.split("#filter/")[1].trim();
+
+        try {
+          filters = JSON.parse(url);
+        }
+
+        catch(err) {
+          window.location.hash="#";
+          return;
+        }
+
+        renderFilterResults(filters, area);
+      }
+    };
+
+    if(map[temp]){
+      map[temp]();
+    } else {
+      renderErrorPage();
+    }
+  }
+
+  function renderAreaPage() {
+    let page = $('.select-area');
+  }
+
+  function renderAnalyticsChart(inputArea) {
+    let page = $('.all-analytics');
+  }
+
+  function renderFilterResults(inputFilters, inputArea){
+    let criteria = ['analytics', 'property-type'];
+    let results = [];
+    let isFiltered = false;
+
+    checkboxes.prop("checked", false);
+
+    criteria.forEach(function (criterion) {
+      if(filters[criterion] && filters[criterion].length){
+        if(isFiltered){
+          area = results;
+          results = [];
+        }
+
+      }
+    });
+
+    renderAnalyticsChart(inputArea);
+  }
+
   function renderErrorPage() {
-    let page = $('.error');
-    page.addClass('visible');
+    let page = $(".error");
+    page.addClass("visible");
   }
 
   function createQueryHash(queryFilters) {
     if(!$.isEmptyObject(queryFilters)) {
-      window.location.hash = '#filter/' + JSON.stringify(queryFilters);
+      window.location.hash = "#filter/" + JSON.stringify(queryFilters);
     } else {
-      window.location.hash = '#';
+      window.location.hash = "#";
     }
   }
+
 });
