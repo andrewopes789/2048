8336b3fad20d80d247befd3bf1ec230065f25e77
Andrew Cho <andrewopes789@gmail.com>
2018-03-04 16:29:23 -0800

    Update api key

--- assets/javascript/api_util/fetch_data.js
+++ assets/javascript/api_util/fetch_data.js
@@ -6,7 +6,7 @@ export const fetchAnalytics = (zipCode, year=2017) => (
     url:
     `https://search.onboard-apis.com/propertyapi/v1.0.0/salestrend/snapshot?geoid=ZI${zipCode}&interval=monthly&startyear=${year}&endyear=${year}&startmonth=january&endmonth=december`,
     headers: {
-      apikey: 'df2bd3344e508b7456d06799d8e08f44',
+      apikey: '2fd32fee9e14d8088d4675daa34e1332',
       Accept: 'application/json'
     }
   })
